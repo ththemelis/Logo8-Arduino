1c26efb51866fa7aee89adf8b45028974c12c05c
Theo Themelis <themelis@sch.gr>
2019-10-23 09:28:10 +0300

    Update Logo8-Arduino.ino

--- Logo8-Arduino.ino
+++ Logo8-Arduino.ino
@@ -1,69 +1,94 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include "MgsModbus.h" // Ίσως θα ήταν καλύτερο να χρησιμοποιηθεί η βιβλιοθήκη ArduinoModbus
+#include "MgsModbus.h"
 
 MgsModbus Mb;
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x94, 0xB5 }; // Ethernet MAC Address
-byte ip[]={192, 168, 1, 150};
-byte sub[]={255, 255, 255, 255};
+byte ip[] = {192, 168, 1, 150};
+byte sub[] = {255, 255, 255, 255};
 
 int potPin0 = 0;    // Αναλογική πόρτα 0
 int potPin1 = 1;    // Αναλογική πόρτα 1
-int potPin2 = 2;    // Αναλογική πόρτα 2
-int val0 = 0;       
-int val1 = 0;       
-int val2 = 0; 
-int relay0 = 4;      
+int potPin2 = 2;    // Αναλογική πόρτα 1
+
+int relay0 = 4;
 int relay1 = 5;
 int relay2 = 6;
 int relay3 = 7;
+int relay4 = 8;
+int relay5 = 9;
 
 void setup(){
   Serial.begin (9600);
 
+  pinMode(potPin0, INPUT);
+  pinMode(potPin1, INPUT);
+  pinMode(potPin2, INPUT);
   pinMode(relay0, OUTPUT);
   pinMode(relay1, OUTPUT);
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
-
-  digitalWrite(relay0, HIGH);
-  digitalWrite(relay1, HIGH);
-  digitalWrite(relay2, HIGH);
-  digitalWrite(relay3, HIGH);
-  /*if (Ethernet.begin(mac) == 0) {
-    Serial.println("Δεν ήταν δυνατή η αρχικοποίηση της σύνδεσης στο διαδίκτυο μέσω DHCP");
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) { // Έλεγχος ύπαρξης του Ethernet Shield
-      Serial.println("Δεν υπάρχει ή δεν λειτουργεί το Ethernet Shield");
-    } // Πρέπει να μπει μια σημαία που να υποδεικνύει ότι δεν υπάρχει το Ethernet Shield
-    if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Δεν είναι συνδεδεμένο το καλώδιο.");
-    } // Πρέπει να μπει μια σημαία που να υποδεικνύει ότι δεν υπάρχει καλώδιο σύνδεσης στο δίκτυο
-    Ethernet.begin(mac, ip); // Αρχικοποίηση της σύνδεσης στο διαδίκτυο με στατική ΙΡ
-    Serial.println(Ethernet.localIP());
-    Mb.remSlaveIP = (192,168,1,201);
-  }*/
+  pinMode(relay4, OUTPUT);
+  pinMode(relay5, OUTPUT);
+  Mb.remSlaveIP = (192,168,1,201);
+  Ethernet.begin(mac, ip, sub);
 }
 
 void loop(){
-  //Serial.println(Mb.MbData[0]);
-  //Mb.MbsRun();
-  val0 = analogRead(potPin0);
-  val1 = analogRead(potPin1);
-  val2 = analogRead(potPin2);    
-  Serial.print ("val0=");
-  Serial.println (val0);
-  Serial.print ("val1=");
-  Serial.println (val1);
-  Serial.print ("val2=");
-  Serial.println (val2); 
+  Serial.print ("ModBus[0]: ");
+  Serial.println(Mb.MbData[0]); // Ανάγνωση από τον δίαυλο ModBus. Τα δεδομένα έρχονται σε δυαδική μορφή
+  Serial.print ("Bit[0]: ");
+  Serial.println(bitRead(Mb.MbData[0],0));
+  Serial.print ("Bit[2]: ");
+  Serial.println(bitRead(Mb.MbData[0],3));  
 
-  if (val0 >= 500){
+  if (bitRead(Mb.MbData[0],0) == 1){
+    digitalWrite(relay0, LOW);
+  }
+  else {
+    digitalWrite(relay0, HIGH);
+  }
+
+  if (bitRead(Mb.MbData[0],1) == 1){
     digitalWrite(relay1, LOW);
   }
   else {
     digitalWrite(relay1, HIGH);
   }
-     
-  delay (500);
+
+  if (bitRead(Mb.MbData[0],2) == 1){
+    digitalWrite(relay2, LOW);
+  }
+  else {
+    digitalWrite(relay2, HIGH);
+  }
+
+  if (bitRead(Mb.MbData[0],3) == 1){
+    digitalWrite(relay3, LOW);
+  }
+  else {
+    digitalWrite(relay3, HIGH);
+  }      
+
+  if (bitRead(Mb.MbData[0],4) == 1){
+    digitalWrite(relay4, LOW);
+  }
+  else {
+    digitalWrite(relay4, HIGH);
+  }
+
+  if (bitRead(Mb.MbData[0],5) == 1){
+    digitalWrite(relay5, LOW);
+  }
+  else {
+    digitalWrite(relay5, HIGH);
+  }
+
+  Mb.MbData[4] = analogRead (potPin0); // Εγγραφή στην θέση 5 του ModBus Address
+  Mb.MbData[5] = analogRead (potPin1); // Εγγραφή στην θέση 6 του ModBus Address
+  Mb.MbData[6] = analogRead (potPin2); // Εγγραφή στην θέση 7 του ModBus Address
+  
+  Mb.MbsRun(); // Λειτουργία του Arduino σε Slave mode
+  delay (100);
 }
